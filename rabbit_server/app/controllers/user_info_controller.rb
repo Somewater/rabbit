fc1327449bb1c6057b8510046c6e0fff932fb4a0
Somewater <mktsz@mail.ru>
2011-12-17 19:36:34 +0400

    fix Ошибка при получении ревардов друга

--- rabbit_server/app/controllers/user_info_controller.rb
+++ rabbit_server/app/controllers/user_info_controller.rb
@@ -2,7 +2,7 @@ class UserInfoController < BaseUserController
 	# прогнать левел инстанс через server_logic и выдать ответ клиенту
 	def process
 		raise FormatError, "Request has bad format" unless @json['user'] && @json['user']['uid']
-		@personage = User.find_by_uid(@json['user']['uid'], @json['net'])
+		@personage = User.find_by_uid(@json['user']['uid'], @params['net'])
 		raise LogicError, "User uid #{@json['user']['uid']} not found" unless @personage
 
 		@response['info'] = @personage.to_json
