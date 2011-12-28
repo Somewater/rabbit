7d1fb0b07acc0f111e526651606134b07fc16149
Root <Root@email>
2011-12-28 10:01:07 +0000

    Превент ошибки, когда клиент неправильно рассчитал награды (выдал меньше, чем надо)

--- rabbit_server/app/controllers/levels_controller.rb
+++ rabbit_server/app/controllers/levels_controller.rb
@@ -14,12 +14,14 @@ class LevelsController < BaseUserController
 		client_rewards.each do |r|
 			# стоило бы сделать проверку, является ли ur вновь созданным ревардом, но неважно
 			# (т.е. можно лишь схакать новое положение для награды)
-			ur = @user.rewards[r['id'].to_s]
-			ur['x'] = r['x']
-			ur['y'] = r['y']
-			li_r = rewards.find{|lir| lir.id.to_s == r['id'].to_s }
-			li_r.x = r['x']
-			li_r.y = r['y']
+			if(r)
+				ur = @user.rewards[r['id'].to_s]
+				ur['x'] = r['x']
+				ur['y'] = r['y']
+				li_r = rewards.find{|lir| lir.id.to_s == r['id'].to_s }
+				li_r.x = r['x']
+				li_r.y = r['y']
+			end
 		end
 
 		@response['levelInstance'] = level_instance.to_json
