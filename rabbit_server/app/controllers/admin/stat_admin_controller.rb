4716eab4793858d75263d4908e90781afba71a13
Root <Root@email>
2012-01-27 16:44:38 +0000

    Stat admin multi view draft

--- rabbit_server/app/controllers/admin/stat_admin_controller.rb
+++ rabbit_server/app/controllers/admin/stat_admin_controller.rb
@@ -1,5 +1,7 @@
 class StatAdminController < AdminController::Base
 	STAT_PATH = '/admin/stat'
+	
+	Time::DATE_FORMATS[:dd_mm_yyyy] = "%d.%m.%Y"
 
 	def check_permissions()
 		raise AuthError, "Illegal operation" unless @admin_user.can?(AdminUser::PERMISSION_STAT_VIEW)
@@ -11,12 +13,31 @@ class StatAdminController < AdminController::Base
 
 	def call
 		@names = Stat.select('name').group('name').map{|s| s.name }
+		
+		@opt = @request['opt'] || {}
+		
+		@opt['names'] ||= @request['names'] || []
+		@opt['names'] = @opt['names'].split(',')	unless @opt['names'].is_a? Array
+		
+		@opt['from'] = string_date_to_time(@opt['from'], Time.new(2012,1,1))
+		@opt['to'] = string_date_to_time(@opt['to'], Time.new)
+		
 
-		if(@request['name'])
-			@name = @request['name']
-			@stats = Stat.all(:conditions => ['name = ?', @name], :order => 'time DESC')
+		if(@opt['names'] && @opt['names'].size > 0)
+			#@name = @request['name']
+			#@stats = Stat.all(:conditions => ['name = ?', @name], :order => 'time DESC')
+			@stats =  Stat.find_by_params(@opt['names'], @opt['from'], @opt['to'])
 		end
 
 		template(File.read("#{TEMPLATE_ROOT}/admin/stat_admin_show.erb"))
+	end
+	
+	# date - timestamp (sec), array [<day>,<month>,<year>], string "28.12.2011"
+	def string_date_to_time(date, default)
+		return nil unless date
+		return Time.at(date.to_s.to_i) if (date.to_s =~ /^\d{9,11}$/) == 0
+		date = date.split(',').map{|i| i.to_i} if date.is_a? String
+		date = Time.new(date[0] || default.day, date[1] || default.month, date[2] || default.year) if date.is_a? Array && date.size > 0
+		date
 	end
 end
--- rabbit_server/app/models/stat.rb
+++ rabbit_server/app/models/stat.rb
@@ -24,6 +24,24 @@ class Stat < ActiveRecord::Base
 		self.count_by_sql(["INSERT INTO stats SET name=?, time=#{self.time}, value=? ON DUPLICATE KEY UPDATE value=value+?",name,diff,diff])
 	end
 	
+	# names = array of string
+	# from,to - Time
+	def self.find_by_params(names, from = nil, to = nil)
+		def time_to_str(time)
+			i =	time.to_i
+			i - (i % 7200)
+		end
+		time_conditions = ''
+		if(from && to)
+			time_conditions = ' AND time > #{time_to_str(from)} AND time < #{time_to_str(to)}'
+		elsif from
+			time_conditions = ' AND time > #{time_to_str(from)}'	
+		elsif to
+			time_conditions = ' AND time < #{time_to_str(to)}'
+		end
+		Stat.find(:all, :conditions => ["name in (?)#{time_conditions}", names.map{|n| "'#{n}'"}.join(',')])
+	end
+	
 	private
 	def self.time
 		ts = Application.time.to_i
