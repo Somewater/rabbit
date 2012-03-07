4ace9f19b28d2faf080033691ff869966d4fb09a
Somewater <mktsz@mail.ru>
2012-03-07 11:26:55 +0400

    Фикс топ контроллера для работоспособности во многих сетях

--- rabbit_server/app/controllers/top_controller.rb
+++ rabbit_server/app/controllers/top_controller.rb
@@ -5,18 +5,20 @@ class TopController < BaseController
 
 	# просто выдает всю информацию, предварительно сохраненную в TopManager
 	def process
+		net = @params['net'].to_i
 		# если надо, обновить кэш
-		if(@@cache == nil || @@cache_version != TopManager.instance.top_version)
+		if(@@cache == nil || @@cache[net] == nil || @@cache_version != TopManager.instance.top_version)
 			@@cache_version = TopManager.instance.top_version
-			@@cache = {}
+			@@cache = {} unless @@cache
+			@@cache[net] = {} unless @@cache[net]
 			TopManager::TOP_NAMES.each do |top_name|
-				top = TopManager.instance.get_tops(@params['net'], top_name)
+				top = TopManager.instance.get_tops(net, top_name)
 				top_data = ""
 				top.each{|t| top_data << t[:uid].to_s << ';' << t[:value].to_s << ';' }
-				@@cache[top_name] = top_data # todo: при необходимости ужать данные (value -> v, uid -> u)
+				@@cache[net][top_name] = top_data # todo: при необходимости ужать данные (value -> v, uid -> u)
 			end
 		end
-		@response = @@cache
+		@response = @@cache[net]
 	end
 
 end
