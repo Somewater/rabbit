18b903e17e715a611ad4466474ed6d9091df11cc
pav <mktsz@mail.ru>
2013-08-04 20:07:46 +0400

    level.xml cache all content page

--- rabbit_server/app/controllers/level_xml_generator.rb
+++ rabbit_server/app/controllers/level_xml_generator.rb
@@ -1,17 +1,19 @@
 module LevelXmlGenerator
 
+	@@request_caches = {}
+
 	def self.generate(release)
-		head_levels = Level.all_head
-		if(release)
-			head_levels.delete_if{|l| l.number > 99}
-		end
-		levels_xml = head_levels.map{|lvl| lvl.to_xml }
-		stories_xml = Story.all_head.map{|story| story.to_xml}
-		offers_xml = OfferManager.instance.all_by_id.map{|id, offer| offer.to_xml }
-		[
-		 200,
-		 {"Content-Type" => "text/xml; charset=UTF-8"},
-		 "<?xml version=\"1.0\" encoding=\"UTF-8\"?><data>
+		request = 'release=' << (release ? 't' : 'f')
+		content = @@request_caches[request] ||= begin
+			Application.logger.warn("REGENERATE TO #{request}")
+			head_levels = Level.all_head
+			if(release)
+				head_levels.delete_if{|l| l.number > 99}
+			end
+			levels_xml = head_levels.map{|lvl| lvl.to_xml }
+			stories_xml = Story.all_head.map{|story| story.to_xml}
+			offers_xml = OfferManager.instance.all_by_id.map{|id, offer| offer.to_xml }
+			"<?xml version=\"1.0\" encoding=\"UTF-8\"?><data>
 <stories>
 #{stories_xml.join("\n")}
 </stories>
@@ -22,6 +24,11 @@ module LevelXmlGenerator
 #{offers_xml.join("\n")}
 </offers>
 </data>"
-		]
+			end
+		[200,{"Content-Type" => "text/xml; charset=UTF-8"},content]
+	end
+
+	def self.clear_cache()
+		@@request_caches = {}
 	end
 end
