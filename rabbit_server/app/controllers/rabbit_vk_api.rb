e9951ab3e44efe112eb14fc081a6f1a9cf191fbf
Pavel Naydenov <naydenov.p.v@gmail.com>
2013-09-01 21:51:01 +0400

    rabbit vk payment implementation

---
+++ rabbit_server/app/controllers/rabbit_vk_api.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+
+require_relative 'net/vk_api'
+class RabbitVkApi < VkApi
+	def get_item_info item, lang
+		money = CONFIG[self.name.to_s]["netmoney_to_money"][item.to_i]
+		{:title => "Круглики", :price => item.to_i}
+	end
+
+	# Осуществить покупку товара
+	# @param item:String идентификатор товара
+	# @return Int уникальный номер заказа в системе
+	def order_status_change receiver_id, item, net_price
+		user = User.find_by_uid(receiver_id)
+		netmoney_to_money = CONFIG[self.name.to_s]["netmoney_to_money"]
+		raise "Current net unsupport billing" unless netmoney_to_money && netmoney_to_money[net_price.to_i]
+		money = netmoney_to_money[net_price.to_i]
+		t = Transaction.create_from(user, money, net_price).save
+		t.save
+		user.money += money
+		user.save
+		t.id
+	end
+end
+
+NetApi.register(RabbitVkApi)
